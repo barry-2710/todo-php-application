3778abba1efe85301f0616b0bf37af4b42c303b6
barry-2710 <73114649+barry-2710@users.noreply.github.com>
2020-12-13 20:05:04 +0530

    small changer

--- navbar.php
+++ navbar.php
@@ -2,7 +2,7 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="viewport" content="width=1024">
      <!-- Fonts -->
      <link rel="dns-prefetch" href="//fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
